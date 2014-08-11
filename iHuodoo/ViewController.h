dcfb8a7dcee74f462e3fb1080d0412d9a473b794
daasan123 <daasan123@163.com>
2014-08-11 12:08:21 +0800

    Initial Commit

---
+++ iHuodoo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  iHuodoo
+//
+//  Created by 伍 兵 on 14-8-11.
+//  Copyright (c) 2014年 伍 兵. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
